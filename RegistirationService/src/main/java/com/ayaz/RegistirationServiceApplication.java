bd48f312b6187c6d83c485a8f9f67de6f48cb3ff
ayaznacafli <ayaz.nacafli@mail.ru>
2020-05-19 19:34:12 +0300

    created Registiration Service App

---
+++ RegistirationService/src/main/java/com/ayaz/RegistirationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ayaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistirationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistirationServiceApplication.class, args);
+	}
+
+}
---
+++ RegistirationService/src/test/java/com/ayaz/RegistirationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistirationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
